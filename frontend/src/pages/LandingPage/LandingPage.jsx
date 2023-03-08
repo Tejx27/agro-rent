d354547e55a0bddf1318ad43718e7d928b4b6ddc
Tejas Nikam <Tejasnikam4515@gmail.com>
2023-03-08 18:27:34 +0530

    add search & aligned cards

--- frontend/src/pages/LandingPage/LandingPage.jsx
+++ frontend/src/pages/LandingPage/LandingPage.jsx
@@ -1,56 +1,61 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Header, Sidebar } from "../../components";
+import axios from "axios";
 import "./landingpage.css";
-import tools from "../../tool-data.json";
-// const tools = [
-//   {
-//     "id": 1,
-//     "name": "Hand Trowel",
-//     "category": "Hand Tools",
-//     "price": "$10",
-//     "description": "A small handheld tool used for digging, planting, and transplanting small plants."
-//   },
-//   {
-//     "id": 2,
-//     "name": "Pruning Shears",
-//     "category": "Hand Tools",
-//     "price": "$20",
-//     "description": "A tool used for cutting and trimming plants, shrubs, and small tree branches."
-//   },
-//   {
-//     "id": 3,
-//     "name": "Hoe",
-//     "category": "Garden Tools",
-//     "price": "$15",
-//     "description": "A tool with a flat blade used for breaking up soil and removing weeds."
-//   }
-// ];
+
 const LandingPage = () => {
-  
+  const [tools, setTools] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5500/showallproducts/allproducts")
+      .then((response) => {
+        setTools(response.data);
+      });
+  }, []);
+
+  const handleSearch = (term) => {
+    setSearchTerm(term);
+  };
+
   return (
-      <div className="landing-page">
-        <Header />
-        <Sidebar />
-        <div className="landing-page-content">
-          {tools.map((tool) => (
-            <div className="landing-page-content-card" key={tool.id}>
+    <div className="landing-page">
+      <Header onSearch={handleSearch} />
+      <Sidebar />
+      <div className="landing-page-content">
+        {tools
+          .filter((tool) =>
+            tool.toolName.toLowerCase().includes(searchTerm.toLowerCase()) || tool.toolCategory.toLowerCase().includes(searchTerm.toLowerCase())
+          )
+          .map((tool, index) => (
+            <div
+              className="landing-page-content-card"
+              key={tool._id}
+              style={{ marginLeft: index % 2 !== 0 ? "1rem" : "0" }}
+            >
               <div className="landing-page-content-card-header">
-                <img src={`https://fmexim.com/images/asset-agri-tools-sickle.jpg`} alt="img" />
+                <img
+                  src={`https://fmexim.com/images/asset-agri-tools-sickle.jpg`}
+                  alt="img"
+                />
                 <div className="name-heading">
-                  <span className="heading">{tool.name}</span>
-                  <span className="category">Category: {tool.category}</span>
+                  <span className="heading">{tool.toolName}</span>
+                  <span className="category">
+                    Category: {tool.toolCategory}
+                  </span>
                 </div>
-                <span className="pricing">{tool.price}/day</span>
+                <span className="pricing">{tool.toolPrice}/day</span>
               </div>
               <div className="discription">
                 <span className="discription-header">Description</span>
-                <p>{tool.description}</p>
+                <p>{tool.toolDesc}</p>
               </div>
             </div>
           ))}
-        </div>
       </div>
-    );
-  };
+    </div>
+  );
+};
 
 export default LandingPage;
